56da27996e6d6b41194306c48a3de4dc59776acc
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-15 19:50:29 +0800

    1.4v

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const btnGenerate = document.querySelector("#generateBtn");
 const textInput = document.querySelector(".inputText");
 const passLength = document.getElementById("passLength");
 const passLengthValue = document.querySelector(".passLengthValue");
-const passIdentifier = document.querySelector(".passIdentifier");
+
 
 const checkboxNum = document.querySelector("#checkboxNum");
 const checkboxSym = document.querySelector("#checkboxSym");
@@ -36,18 +36,5 @@ btnGenerate.addEventListener("click", () => {
   for (i = 0; i < passLength.value; i++) {
     let generatePass = Math.floor(Math.random() * totalRandomChar.length);
     textInput.value += totalRandomChar[generatePass];
-  }
-
-  passIdentifier.style.cssText += "display:block;padding: 0.5rem";
-
-  if (passLength.value <= 11) {
-    passIdentifier.textContent = "WEAK";
-    passIdentifier.style.backgroundColor = "#ef233c";
-  } else if (passLength.value <= 16) {
-    passIdentifier.textContent = "STRONG";
-    passIdentifier.style.backgroundColor = "#ffe14c";
-  } else {
-    passIdentifier.textContent = "VERY STRONG";
-    passIdentifier.style.backgroundColor = "#74c69d";
   }
 });
