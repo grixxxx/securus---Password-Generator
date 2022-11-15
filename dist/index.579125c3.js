4de6d309c3e4634ac38d60ac520184854e8124ab
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-15 23:06:03 +0800

    update

--- dist/index.579125c3.js
+++ dist/index.579125c3.js
@@ -2,14 +2,13 @@ const btnGenerate = document.querySelector("#generateBtn");
 const textInput = document.querySelector(".inputText");
 const passLength = document.getElementById("passLength");
 const passLengthValue = document.querySelector(".passLengthValue");
-const passIdentifier = document.querySelector(".passIdentifier");
 const checkboxNum = document.querySelector("#checkboxNum");
 const checkboxSym = document.querySelector("#checkboxSym");
 const checkboxLowLetter = document.querySelector("#checkboxLowLetter");
 textInput.value = "";
-passLengthValue.textContent = passLength.value;
+passLengthValue.textContent = passLength.value + " characters";
 passLength.oninput = function() {
-    passLengthValue.textContent = this.value;
+    passLengthValue.textContent = this.value + " characters";
 };
 let totalRandomChar = "";
 btnGenerate.addEventListener("click", ()=>{
@@ -24,17 +23,6 @@ btnGenerate.addEventListener("click", ()=>{
     for(i = 0; i < passLength.value; i++){
         let generatePass = Math.floor(Math.random() * totalRandomChar.length);
         textInput.value += totalRandomChar[generatePass];
-    }
-    passIdentifier.style.cssText += "display:block;padding: 0.5rem";
-    if (passLength.value <= 11) {
-        passIdentifier.textContent = "WEAK";
-        passIdentifier.style.backgroundColor = "#ef233c";
-    } else if (passLength.value <= 16) {
-        passIdentifier.textContent = "STRONG";
-        passIdentifier.style.backgroundColor = "#ffe14c";
-    } else {
-        passIdentifier.textContent = "VERY STRONG";
-        passIdentifier.style.backgroundColor = "#74c69d";
     }
 });
 
