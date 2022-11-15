498c986f40b60bd6ba4984ae6ad40f1ce95062bc
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-15 15:50:32 +0800

    change password Length style

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
 const btnGenerate = document.querySelector("#generateBtn");
 const textInput = document.querySelector(".inputText");
 const passLength = document.getElementById("passLength");
+const passLengthValue = document.querySelector(".passLengthValue");
 const passIdentifier = document.querySelector(".passIdentifier");
 
 let letterChar =
   "jklzxcvbnm1234567890qwertyuiopasdfghQWERTYUIOPASD!@#$%^&*()_+<>?FGHJKLZXCVBNM";
 
 textInput.value = "";
-passLength.value = "";
+
+passLengthValue.textContent = passLength.value;
+
+passLength.oninput = function () {
+  passLengthValue.textContent = this.value;
+};
 
 btnGenerate.addEventListener("click", () => {
   textInput.value = "";
 
-  if (passLength.value == 0) {
-    alert("Please Input Password length");
-  } else if (passLength.value >= 21) {
-    alert("Max Password length is 20 letters");
-    passLength.value = "";
-    passLength.style.cssText = "border: 2px solid red";
+  for (i = 0; i < passLength.value; i++) {
+    let generatePass = Math.floor(Math.random() * letterChar.length);
+    textInput.value += letterChar[generatePass];
+  }
+  passLength.style.border = "none";
+
+  passIdentifier.style.cssText += "display:block;padding: 0.5rem";
+
+  if (passLength.value <= 11) {
+    passIdentifier.textContent = "WEAK";
+    passIdentifier.style.backgroundColor = "#ef233c";
+  } else if (passLength.value <= 16) {
+    passIdentifier.textContent = "STRONG";
+    passIdentifier.style.backgroundColor = "#ffe14c";
   } else {
-    for (i = 0; i < passLength.value; i++) {
-      let generatePass = Math.floor(Math.random() * letterChar.length);
-      textInput.value += letterChar[generatePass];
-    }
-    passLength.style.border = "none";
-
-    passIdentifier.style.cssText += "display:block;padding: 0.5rem";
-
-    if (passLength.value <= 12) {
-      passIdentifier.textContent = "WEAK";
-      passIdentifier.style.backgroundColor = "#ef233c";
-    } else if (passLength.value <= 18) {
-      passIdentifier.textContent = "STRONG";
-      passIdentifier.style.backgroundColor = "#ffe14c";
-    } else {
-      passIdentifier.textContent = "VERY STRONG";
-      passIdentifier.style.backgroundColor = "#74c69d";
-    }
+    passIdentifier.textContent = "VERY STRONG";
+    passIdentifier.style.backgroundColor = "#74c69d";
   }
 });
