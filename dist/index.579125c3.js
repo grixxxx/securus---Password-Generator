38154384f21363486fb7e153113fd0dc8774a037
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-16 11:33:15 +0800

    update for netlify deployment

--- dist/index.579125c3.js
+++ dist/index.579125c3.js
@@ -5,6 +5,7 @@ const passLengthValue = document.querySelector(".passLengthValue");
 const checkboxNum = document.querySelector("#checkboxNum");
 const checkboxSym = document.querySelector("#checkboxSym");
 const checkboxLowLetter = document.querySelector("#checkboxLowLetter");
+const checkboxUpperLetter = document.querySelector("#checkboxUpperLetter");
 textInput.value = "";
 passLengthValue.textContent = passLength.value + " characters";
 passLength.oninput = function() {
@@ -13,9 +14,16 @@ passLength.oninput = function() {
 let totalRandomChar = "";
 btnGenerate.addEventListener("click", ()=>{
     textInput.value = "";
-    if (checkboxSym.checked && checkboxNum.checked) totalRandomChar = checkboxNum.value + checkboxSym.value + checkboxLowLetter.value;
+    if (checkboxUpperLetter.checked && checkboxSym.checked && checkboxNum.checked) {
+        console.log(true);
+        totalRandomChar = checkboxSym.value + checkboxUpperLetter.value + checkboxNum.value;
+    } else if (checkboxUpperLetter.checked && checkboxSym.checked) totalRandomChar = checkboxSym.value + checkboxUpperLetter.value;
+    else if (checkboxSym.checked && checkboxNum.checked) totalRandomChar = checkboxNum.value + checkboxSym.value + checkboxLowLetter.value;
+    else if (checkboxUpperLetter.checked && checkboxSym.checked && checkboxNum.checked) totalRandomChar = checkboxSym.value + checkboxUpperLetter.value + checkboxNum.value;
+    else if (checkboxUpperLetter.checked && checkboxNum.checked) totalRandomChar = checkboxNum.value + checkboxUpperLetter.value;
     else if (checkboxSym.checked) totalRandomChar = checkboxSym.value + checkboxLowLetter.value;
     else if (checkboxNum.checked) totalRandomChar = checkboxNum.value + checkboxLowLetter.value;
+    else if (checkboxUpperLetter.checked) totalRandomChar = checkboxUpperLetter.value;
     else {
         checkboxLowLetter.checked;
         totalRandomChar = checkboxLowLetter.value;
